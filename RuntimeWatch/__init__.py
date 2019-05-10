cdda944ec53115a99109af74d4cd982e46ea870e
mrstephenneal <stephen.neal14@gmail.com>
2019-05-10 09:53:14 -0400

    REFACTOR TaskTracker to RuntimeWatch

---
+++ RuntimeWatch/__init__.py
@@ -0,0 +1,4 @@
+from RuntimeWatch.runtime import get_json, most_recent_history, TaskTracker
+
+
+__all__ = ['get_json', 'most_recent_history', 'TaskTracker']
--- TaskTracker/__init__.py
+++
@@ -1,4 +0,0 @@
-from TaskTracker.tracker import get_json, most_recent_history, TaskTracker
-
-
-__all__ = ['get_json', 'most_recent_history', 'TaskTracker']
